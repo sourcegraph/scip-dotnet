2494338e1f8c0239f84ec2a5f7744d16550d9880
AraGimLeg <sagi1623@gmail.com>
2023-06-20 12:33:29 +0200

    Add test case for VbNet with different casing (#32)

---
+++ snapshots/input/syntax/VBMain/CaseInsensitive.vb
@@ -0,0 +1,7 @@
+﻿Namespace VBMain
+    Public Class CaseInsensitive
+        Public Sub DifferentCase(wEiRdCaSiNg As String)
+            Console.WriteLine(WeIrDcAsInG)
+        End Sub
+    End Class
+End Namespace
