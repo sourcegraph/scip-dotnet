9d271ecfb3a3779f5eb19e00984903f3fc18b33a
AraGimLeg <sagi1623@gmail.com>
2023-06-20 22:21:22 +0200

    Add WPF application as a project that targets windows

---
+++ snapshots/input/WpfApplication/WpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace WpfApplication
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ snapshots/output-net6.0/WpfApplication/WpfApplication/App.xaml.cs
@@ -0,0 +1,12 @@
+  using System.Windows;
+
+  namespace WpfApplication
+//          ^^^^^^^^^^^^^^ reference scip-dotnet nuget . . WpfApplication/
+  {
+      public partial class App : Application
+//                         ^^^ definition scip-dotnet nuget . . WpfApplication/App#
+//                             documentation ```cs\nclass App\n```
+//                             relationship implementation scip-dotnet nuget . . ``/Application#
+      {
+      }
+  }
